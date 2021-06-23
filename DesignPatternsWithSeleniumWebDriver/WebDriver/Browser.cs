10db53a00bc5e7ab6780185049d5b64082bfb4f1
Iryna Zybliyenka <Iryna_Zybliyenka@epam.com>
2021-06-23 17:02:23 +0300

    Adding tests for yandex mail that use page object and factory patterns

---
+++ DesignPatternsWithSeleniumWebDriver/WebDriver/Browser.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace DesignPatternsWithSeleniumWebDriver.WebDriver
+{
+    public class Browser
+    {
+        private static IWebDriver driver;
+        private static Browser currentInstance;
+
+        public Browser()
+        {
+            var service = FirefoxDriverService.CreateDefaultService();
+            driver = new FirefoxDriver(service);
+        }
+
+        public static Browser Instance => currentInstance ?? (currentInstance = new Browser());
+
+        public static void NavigateTo(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public static void MaximizeWindow()
+        {
+            driver.Manage().Window.Maximize();
+        }
+
+        public static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+        public static void Quit()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ DesignPatternsWithSeleniumWebDriver/WebObjects/SignInAndOutPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+
+namespace DesignPatternsWithSeleniumWebDriver.WebObjects
+{
+    public class SignInAndOutPage : BasePage
+    {
+        private static readonly By signInForm = By.ClassName("passp-auth-content");
+
+        public SignInAndOutPage() : base(signInForm, "SignIn Page") { }
+
+        //Lambda expression is used for Page Factory pattern as FindsBy is no more used
+        BaseElement loginField => new BaseElement(By.Id("passp-field-login"));
+        BaseElement signInButton => new BaseElement(By.XPath("//button[@class='Button2 Button2_size_l Button2_view_action Button2_width_max Button2_type_submit']"));
+        BaseElement passwordField => new BaseElement(By.Id("passp-field-passwd"));
+        BaseElement actualLoginMessage => new BaseElement(By.XPath("//div[@class='passp-auth-screen passp-welcome-page']/h1/span"));
+
+        public void LoginToEmail()
+        {
+            loginField.SendKeys("Selenium1Web1Driver@yandex.by");
+            signInButton.Click();
+            passwordField.SendKeys("$elenium789");
+            signInButton.Click();
+        }
+
+        public string GetActualLoginMessage()
+        {
+            return actualLoginMessage.GetText();
+        }
+    }
+}
